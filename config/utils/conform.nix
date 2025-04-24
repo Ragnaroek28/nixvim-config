662a58673cd8eaefba241136c522c7e585e2bbe6
Ragnaroek28 <82148873+Ragnaroek28@users.noreply.github.com>
2025-04-25 00:06:19 +0200

    fix: fixed clang-tidy setting

--- config/utils/conform.nix
+++ config/utils/conform.nix
@@ -3,10 +3,19 @@
     enable = true;
     settings = {
       formatters_by_ft = {
-        cpp = [ "clang-tidy --format-style='file'" ];
+        cpp = [ "clang-tidy" ];
+        c = [ "clang-tidy" ];
         nix = [ "nixfmt" ];
         tex = [ "tex-fmt --nowrap" ];
         latex = [ "tex-fmt --nowrap" ];
+      };
+
+      formatters = {
+        clang-tidy = {
+          command = "clang-tidy";
+          args = [ "--format-style=file" "--quiet" "--" ];
+          stdin = false;
+        };
       };
 
       format_on_save = {
