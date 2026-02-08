6bfbf65808b0182dd0c25980fc7362f9ece87492
Ragnaroek28 <82148873+Ragnaroek28@users.noreply.github.com>
2026-02-09 00:16:02 +0100

    fix(lsp/clangd): it should work now
    hopefully

--- config/lsp/clangd.nix
+++ config/lsp/clangd.nix
@@ -1,22 +1,29 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   lsp.servers.clangd = {
     enable = true;
     packageFallback = false;
     config = {
       cmd = [
-          "${pkgs.clang-tools}"
-          "--background-index"
-          "--query-driver=/nix/store/*-clang-wrapper*/bin/clang++"
-        ];
-        filetypes = [
-          "c"
-          "cpp"
-        ];
-        root_markers = [
-          "compile_commands.json"
-          "compile_flags.txt"
-          ".git"
-        ];
+        "${pkgs.clang-tools}/bin/clangd"
+        "--background-index"
+        # "--query-driver=/nix/store/*-clang-wrapper*/bin/clang++"
+        "--clang-tidy"
+        "--log=verbose"
+      ];
+      filetypes = [
+        "c"
+        "cpp"
+      ];
+      root_markers = [
+        "compile_commands.json"
+        "compile_flags.txt"
+        ".git"
+      ];
     };
+  };
+  plugins.clangd-extensions = {
+    enable = true;
+    enableOffsetEncodingWorkaround = true;
   };
 }
