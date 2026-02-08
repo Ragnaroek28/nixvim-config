c3b2e6f3efbab9abc94d71a50bc1d07b56167abe
Ragnaroek28 <82148873+Ragnaroek28@users.noreply.github.com>
2026-02-08 22:12:12 +0100

    edit(plugins/luasnip): little changes

--- config/plugins/luasnip.nix
+++ config/plugins/luasnip.nix
@@ -1,35 +1,45 @@
 {
-    plugins = {
-        luasnip = {
-            enable = true;
-            luaConfig.content = ''
-                vim.cmd[[
-                " Use Enter to expand the snippet
-                imap <silent><expr> <CR> luasnip#expandable() ? '<Plug>luasnip-expand-snippet' : '<CR>'
+  plugins = {
+    luasnip = {
+      enable = true;
+      luaConfig.content = ''
+        vim.cmd[[
+        " Use Enter to expand the snippet
+        imap <silent><expr> <CR> luasnip#expandable() ? '<Plug>luasnip-expand-snippet' : '<CR>'
 
-                " Use Tab to jump through snippets
-                imap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>' 
-                smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+        " Use Tab to jump through snippets
+        imap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>' 
+        smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
 
-                " Use Shift-Tab to jump backwards through snippets
-                imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-                smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-                ]]
-            '';
-            settings = {
-                enable_autosnippets = true;
-                store_selection_keys = "<Tab>";
-            };
-            filetypeExtend = {
-                plaintex = [ "tex" "latex" ];
-                latex = [ "plaintex" "tex" ];
-            };
-            fromLua = [
-                {}
-                { paths = ./luasnip; }
-                { paths = "$HOME/.config/nvim-local/luasnip"; }
-            ];
-        };
-        cmp_luasnip.enable = true;
+        " Use Shift-Tab to jump backwards through snippets
+        imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+        smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+        ]]
+      '';
+      settings = {
+        enable_autosnippets = true;
+        store_selection_keys = "<Tab>";
+        update_events = [
+          "TextChanged"
+          "TextChangedI"
+        ];
+      };
+      filetypeExtend = {
+        plaintex = [
+          "tex"
+          "latex"
+        ];
+        latex = [
+          "plaintex"
+          "tex"
+        ];
+      };
+      fromLua = [
+        { }
+        { paths = ./luasnip; }
+        { paths = "$HOME/.config/nvim-local/luasnip"; }
+      ];
     };
+    cmp_luasnip.enable = true;
+  };
 }
