09cd24bf40ea1b98c4811dfc6be3302cb008aa34
Ragnaroek28 <82148873+Ragnaroek28@users.noreply.github.com>
2026-02-13 12:06:45 +0100

    edit(colorscheme): activated integrations

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -11,17 +11,32 @@
       # };
       disable_underline = true;
       flavour = "mocha";
-      # integrations = {
-      #   cmp = true;
-      #   gitsigns = true;
-      #   mini = {
-      #     enabled = true;
-      #     indentscope_color = "";
-      #   };
-      #   notify = false;
-      #   nvimtree = true;
-      #   treesitter = true;
-      # };
+      integrations = {
+        native_lsp = {
+          enabled = true;
+          virtual_text = {
+            errors = [ "italic" ];
+            warnings = [ "italic" ];
+            hints = [ "italic" ];
+            information = [ "italic" ];
+          };
+          underlines = {
+            errors = [ "underline" ];
+            warnings = [ "underline" ];
+            hints = [ "underline" ];
+            information = [ "underline" ];
+          };
+        };
+        #   cmp = true;
+        gitsigns = true;
+        #   mini = {
+        #     enabled = true;
+        #     indentscope_color = "";
+        #   };
+        notify = false;
+        #   nvimtree = true;
+        treesitter = true;
+      };
       styles = {
         booleans = [
           "bold"
