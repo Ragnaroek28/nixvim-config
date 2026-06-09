75c4d5b9f8a71a8c5c1498c33e45a9b22cf4f86e
Ragnaroek28 <82148873+Ragnaroek28@users.noreply.github.com>
2026-06-09 11:19:53 +0200

    fix(lsp/qmlls): now using the env

--- config/lsp/qmlls.nix
+++ config/lsp/qmlls.nix
@@ -2,6 +2,16 @@ _:
 {
   lsp.servers.qmlls = {
     enable = true;
+    config = {
+      cmd = [
+        "qmlls"
+        "-E"
+      ];
+      root_markers = [
+        ".git"
+        ".qmlls.ini"
+      ];
+    };
   };
   plugins.lsp.servers.qmlls = {
     enable = true;
