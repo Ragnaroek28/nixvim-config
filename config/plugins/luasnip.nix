9b7db3467369a18fdcbea58d04f36afbb15597e4
Ragnaroek28 <82148873+Ragnaroek28@users.noreply.github.com>
2026-08-06 13:24:17 +0200

    add(plugins/luasnip): c uses c++ snippets

--- config/plugins/luasnip.nix
+++ config/plugins/luasnip.nix
@@ -2,20 +2,6 @@
   plugins = {
     luasnip = {
       enable = true;
-      # luaConfig.content = ''
-      #   vim.cmd[[
-      #   " Use Enter to expand the snippet
-      #   imap <silent><expr> <CR> luasnip#expandable() ? '<Plug>luasnip-expand-snippet' : '<CR>'
-
-      #   " Use Tab to jump through snippets
-      #   imap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
-      #   smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
-
-      #   " Use Shift-Tab to jump backwards through snippets
-      #   imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-      #   smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-      #   ]]
-      # '';
       settings = {
         enable_autosnippets = true;
         store_selection_keys = "<Tab>";
@@ -36,6 +22,11 @@
         cpp = [
           "hpp"
           "cxx"
+          "c"
+          "h"
+        ];
+        c = [
+          "cpp"
         ];
       };
       fromLua = [
